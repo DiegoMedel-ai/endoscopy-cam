249b49cbef80fca428e4722ec7657bab6177f31c
DiegoMedel-ai <d.i.e.g.o.nambo123@gmail.com>
2025-01-30 13:02:50 -0600

    first approach of video streaming to web

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, Response, request, jsonify
+import cv2
+import os
+import time
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(1)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/capture', methods=['POST'])
+def capture():
+    success, frame = camera.read()
+    if success:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        filename = f'snapshot_{timestamp}.jpg'
+        IMAGE_FOLDER = os.path.join(os.getcwd(), 'images')
+        if not os.path.exists(IMAGE_FOLDER):
+            os.makedirs(IMAGE_FOLDER, exist_ok=True)
+        filepath = os.path.join(IMAGE_FOLDER, filename)
+        cv2.imwrite(filepath, frame)
+        return jsonify({"message": f"Imagen guardada como {filename}"})
+    else:
+        return jsonify({"message": "Error al capturar la imagen"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
