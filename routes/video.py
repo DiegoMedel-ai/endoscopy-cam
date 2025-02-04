aa588877579855c10f18dd25223adec54eec0b4d
DiegoMedel-ai <d.i.e.g.o.nambo123@gmail.com>
2025-02-03 19:04:32 -0600

    added approach to use gpios

--- routes/video.py
+++ routes/video.py
@@ -2,17 +2,50 @@ from flask import Blueprint, jsonify, Response, request
 import threading
 import cv2
 import os
+import time
 from services.media_handler import MediaHandler
+from dotenv import load_dotenv
 
-video = Blueprint('video', __name__)
+load_dotenv()
 
 # Configuración inicial
+video = Blueprint('video', __name__)
 PROCEDURE_FOLDER = os.path.join(os.getcwd(), 'PROCEDURES')
 os.makedirs(PROCEDURE_FOLDER, exist_ok=True)
 
 media_handler = MediaHandler(PROCEDURE_FOLDER)
 cap = cv2.VideoCapture(0)
 recording_flag = threading.Event()
+
+environment = os.getenv("environment", "dev")
+
+# Configuración del GPIO para el botón
+if environment == "prod":
+    import gpiod
+    BUTTON_PIN = 70
+    chip = gpiod.Chip('gpiochip0')
+    button_line = chip.get_line(BUTTON_PIN)
+    button_line.request(consumer="button", type=gpiod.LINE_REQ_DIR_IN)
+
+button_pressed = False
+
+# Función para leer el botón
+def read_button():
+    global button_pressed
+    while True:
+        if environment == "prod":
+            button_state = button_line.get_value()
+            if button_state == 0 and not button_pressed:
+                button_pressed = True
+                capture() 
+            elif button_state == 1: 
+                button_pressed = False
+        time.sleep(0.1)
+
+if environment == "prod":
+    button_thread = threading.Thread(target=read_button)
+    button_thread.daemon = True
+    button_thread.start()
 
 @video.route('/video_feed')
 def video_feed():
@@ -50,4 +83,6 @@ def stop_recording():
 @video.route('/shutdown', methods=['POST'])
 def shutdown():
     cap.release()
+    if environment == "prod":
+        button_line.release()
     return jsonify({"message": "Cámara liberada y aplicación cerrada."})
